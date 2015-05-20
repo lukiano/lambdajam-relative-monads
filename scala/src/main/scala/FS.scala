6ebdcb57ae8ea07e970c1c5c43ace07e829c794e
Stephan Hoermann <srhoermann@gmail.com>
2015-05-20 10:27:50 +1000

    Adds comments to FS.scala

--- scala/src/main/scala/FS.scala
+++ scala/src/main/scala/FS.scala
@@ -25,15 +25,36 @@ case class FS[A](runFS: File => Result[A]) {
   def flatMap[B](f: A => FS[B]) =
     FS[B]((cwd: File) => runFS(cwd).flatMap(a => f(a).runFS(cwd)))
 
+  /**
+    * Set the error message in a failure case. Useful for providing contextual information without
+    * having to inspect result.
+    *
+    * NB: This discards any existing message.
+    */
   def setMessage(message: String): FS[A] =
     FS((cwd: File) => runFS(cwd).setMessage(message))
 
+  /**
+    * Adds an additional error message. Useful for adding more context as the error goes up the stack.
+    *
+    * The new message is prepended to any existing message.
+    */
   def addMessage(message: String): FS[A] =
     FS((cwd: File) => runFS(cwd).addMessage(message))
 
+  /**
+    * Runs the first operation. If it fails, runs the second operation. Useful for chaining optional operations.
+    *
+    * Returns the error of `self` iff both `self` and `other` fail.
+    */
   def or(other: FS[A]): FS[A] =
     FS((cwd: File) => runFS(cwd) or other.runFS(cwd))
 
+  /**
+    * Like "finally", but only performs the final action if there was an error.
+    *
+    * If `action` fails that error is swallowed and only the initial error is returned.
+    */
   def onException[B](sequel: FS[B]): FS[A] =
     FS((cwd: File) => runFS(cwd).fold(
       x => Result.ok(x),
@@ -43,11 +64,23 @@ case class FS[A](runFS: File => Result[A]) {
       }
     ))
 
+  /**
+    * Ensures that the provided action is always run regardless of if `this` was successful.
+    * Generalizes "finally".
+    *
+    * If `self` was successful and `sequel` fails it returns the failure from `sequel`. Otherwise
+    * the result of `self` is returned.
+    */
   def ensure[B](sequel: FS[B]): FS[A] = for {
     r <- onException(sequel)
     _  <- sequel
   } yield r
 
+  /**
+    * Applies the "during" action, calling "after" regardless of whether there was an error.
+    *
+    * All errors are rethrown. Generalizes try/finally.
+    */
   def bracket[B, C](after: A => FS[B])(during: A => FS[C]): FS[C] = for {
     a <- this
     r <- during(a) ensure after(a)
